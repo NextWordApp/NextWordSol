0deae40f1650d70cc0de528b11b2a85d3065686b
unic <2445942195@qq.com>
2024-08-12 00:33:17 +0800

    refactor the LPCertNFT

--- script/LPTracker.s.sol
+++ script/LPTracker.s.sol
@@ -1,18 +1,18 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {LPTracker} from "../src/LPTracker.sol";
 
 contract CounterScript is Script {
-    Counter public counter;
+    LPTracker public lpTracker;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        lpTracker = new LPTracker();
 
         vm.stopBroadcast();
     }
--- src/LPCertNFT.sol
+++ src/LPCertNFT.sol
@@ -7,175 +7,208 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract LPCertNFT is ERC721, Ownable {
-    error LPCertNFT__InvalidMilestone(string milestone);
-    error LPCertNFT__MaximumQuantityReached(string milestone, uint16 maxSupply);
-    error LPCertNFT__OnlyOwnerOrCollateralOwner(address _opeartor);
+    // 自定义错误用于更明确的错误处理
+    error LPCertNFT__InvalidMilestone(uint256 milestone);
+    error LPCertNFT__MaximumSupplyReached(uint256 milestone, uint16 maxSupply);
+    error LPCertNFT__OnlyOwnerOrAuthorized(address operator);
 
-    event LPCertNFT__NFTMinted(
+    // NFT铸造事件
+    event NFTMinted(
         uint256 indexed tokenId,
         address indexed owner,
-        string indexed milestone,
+        uint256 indexed milestone,
         uint16 holderRank
     );
 
-    uint256 private s_tokenCounter;
-    uint16 private s_defaultMaxSupply;
-    string[] private s_milestones;
+    // 变量定义
+    uint256 private s_tokenCounter; // 已经铸造的NFT总数
+    uint16 private s_defaultMaxSupply; // 每个里程碑的默认最大供应量
+    uint256[] private s_milestones; // 设定的学习进度里程碑: 用户在学习到一定数量的英语单词时（如100, 1000, 3000, 5000, 8000, 9999）
 
-    mapping(uint256 nftIndex => string nftAddress) private s_indexToNft;
-    mapping(string => uint16) private s_maxSupplyCountryNFT;
-    mapping(string => uint16) private s_numCountryNFT;
-    mapping(address user => string[]) private s_userCountriesNft;
+    // 映射定义
+    mapping(uint256 => string) private s_tokenIdToURI; // 每个NFT的URI
+    mapping(uint256 => uint16) private s_maxSupplyMilestoneNFT; // 每个里程碑的最大NFT数量
+    mapping(uint256 => uint16) private s_numMilestoneNFT; // 每个里程碑当前已经铸造的NFT数量
+    mapping(address => uint256[]) private s_userMilestones; // 用户拥有的里程碑记录
 
+    // 构造函数，初始化里程碑和默认最大供应量
     constructor(
-        string[] memory _countries,
+        uint256[] memory _milestones,
         uint16 _defaultMaxSupply
-    ) ERC721("Limited Edition Country NFT", "LECTNFT") Ownable(msg.sender) {
+    ) ERC721("English Learning Milestone NFT", "ELMNFT") Ownable(msg.sender) {
         s_tokenCounter = 0;
-        s_milestones = _countries;
+        s_milestones = _milestones;
         s_defaultMaxSupply = _defaultMaxSupply;
-        setCountriesMaxSupply(_countries, _defaultMaxSupply);
+        _setMilestonesMaxSupply(_milestones, _defaultMaxSupply);
     }
 
-    function isValidCountry(
-        string memory _countryName,
-        string[] memory _coutryList
-    ) private pure returns (bool valid) {
-        for (uint256 i = 0; i < _coutryList.length; i++) {
-            bytes32 countriesHash = keccak256(abi.encodePacked(_coutryList[i]));
-            bytes32 countryNameHash = keccak256(abi.encodePacked(_countryName));
-            if (countriesHash == countryNameHash) {
-                return true;
+    // 验证给定的单词数量是否达到了有效的里程碑
+    function _isValidMilestone(
+        address _user,
+        uint256 _wordCount
+    ) private view returns (bool) {
+        uint256 _floorMilestone = _getFloorMilestone(_wordCount);
+        uint256[] memory _userMilestones = s_userMilestones[_user];
+        for (uint i = 0; i < _userMilestones.length; i++) {
+            if (_userMilestones[i] == _floorMilestone) {
+                return false; // 如果在用户已经 minted 的 Milestones 找到了最近的 milestone, 那就说明离下一个 milestone 还不达标
             }
         }
+        return true; // 说明该 milestone 还未 mint
+        // for (uint256 i = 0; i < s_milestones.length; i++) {
+        //     if (s_milestones[i] <= _wordCount) {
+        //         return true;
+        //     }
+        // }
+        // return false;
     }
 
-    // Need to update the following functions:
+    // 获取用户背单词以后最近的且有效的里程碑
+    function _getFloorMilestone(
+        uint256 _wordCount
+    ) private view returns (uint256) {
+        uint256 floorMilestone = 0;
+        for (uint256 i = 0; i < s_milestones.length; i++) {
+            if (s_milestones[i] <= _wordCount) {
+                floorMilestone = s_milestones[i];
+            }
+        }
+        return floorMilestone;
+    }
+
+    // 公开函数，供授权者铸造NFT
     function mintNft(
         address to,
-        string memory _countryName,
-        string memory _nftAddress
-    ) public OnlyOwnerOrCollaborators {
-        string[] memory countries = s_milestones;
-        if (!isValidCountry(_countryName, countries)) {
-            revert LPCertNFT__InvalidMilestone(_countryName);
+        uint256 _wordCount,
+        string memory _nftURI
+    ) public onlyAuthorized {
+        // 验证里程碑的有效性
+        if (!_isValidMilestone(to, _wordCount)) {
+            revert LPCertNFT__InvalidMilestone(_wordCount);
         }
 
-        uint256 tokenCounter = s_tokenCounter;
+        uint256 _floorMilestone = _getFloorMilestone(_wordCount);
 
-        // Limit the number of NFTs to mint
+        // 检查是否达到最大供应量
         if (
-            s_numCountryNFT[_countryName] >= s_maxSupplyCountryNFT[_countryName]
+            s_numMilestoneNFT[_floorMilestone] >=
+            s_maxSupplyMilestoneNFT[_floorMilestone]
         ) {
-            revert LPCertNFT__MaximumQuantityReached(
-                _countryName,
-                s_maxSupplyCountryNFT[_countryName]
+            revert LPCertNFT__MaximumSupplyReached(
+                _floorMilestone,
+                s_maxSupplyMilestoneNFT[_floorMilestone]
             );
         }
 
-        s_numCountryNFT[_countryName]++;
-        _safeMint(to, tokenCounter);
+        // 更新里程碑对应的NFT数量
+        s_numMilestoneNFT[_floorMilestone]++;
+        uint256 tokenId = s_tokenCounter;
+        _safeMint(to, tokenId);
 
-        s_indexToNft[tokenCounter] = _nftAddress;
+        // 记录NFT的URI及更新总铸造量
+        s_tokenIdToURI[tokenId] = _nftURI;
         s_tokenCounter++;
 
-        // 更新用户已拥有的国家 NFT
-        string[] storage userCountries = s_userCountriesNft[to];
-        userCountries.push(_countryName);
+        // 将里程碑记录添加到用户数据中
+        s_userMilestones[to].push(_floorMilestone);
 
-        emit LPCertNFT__NFTMinted(
-            tokenCounter,
+        // 触发NFT铸造事件
+        emit NFTMinted(
+            tokenId,
             to,
-            _countryName,
-            s_numCountryNFT[_countryName]
+            _floorMilestone,
+            s_numMilestoneNFT[_floorMilestone]
         );
     }
 
+    // 内部函数，返回基础URI
     function _baseURI() internal pure override returns (string memory) {
         return "https://aquamarine-fascinating-grouse-888.mypinata.cloud/ipfs/";
     }
 
+    // 获取特定tokenId的URI
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        //通过 tokenid 获取到对应 IPFS 的路径 映射给前端展示
-        string memory imageURI = string.concat(
-            _baseURI(),
-            s_indexToNft[tokenId]
-        );
-        return string(abi.encodePacked(bytes(imageURI)));
+        return string(abi.encodePacked(_baseURI(), s_tokenIdToURI[tokenId]));
     }
 
     /* GETTER */
+
+    // 获取总铸造的NFT数量
     function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
     }
 
-    function getNumCountryNFT(
-        string memory _country
+    // 获取特定里程碑的铸造信息
+    function getMilestoneInfo(
+        uint256 _wordCount
     ) public view returns (uint16 minted, uint16 maxSupply) {
-        return (s_numCountryNFT[_country], s_maxSupplyCountryNFT[_country]);
+        uint256 _floorMilestone = _getFloorMilestone(_wordCount);
+
+        return (
+            s_numMilestoneNFT[_floorMilestone],
+            s_maxSupplyMilestoneNFT[_floorMilestone]
+        );
     }
 
-    function getUserMintedCountries(
+    // 获取用户已达成的所有里程碑
+    function getUserMilestones(
         address _user
-    ) public view returns (string[] memory countriesNftUserHave) {
-        return s_userCountriesNft[_user];
+    ) public view returns (uint256[] memory) {
+        return s_userMilestones[_user];
     }
 
-    function getCountries()
+    // 获取所有的学习进度里程碑
+    function getMilestones()
         public
         view
-        OnlyOwnerOrCollaborators
-        returns (string[] memory countries)
+        onlyAuthorized
+        returns (uint256[] memory)
     {
         return s_milestones;
     }
 
     /* SETTER */
-    function setCountries(
-        string[] memory _countries
-    ) public OnlyOwnerOrCollaborators {
-        s_milestones = _countries;
+
+    // 设置新的里程碑
+    function setMilestones(uint256[] memory _milestones) public onlyAuthorized {
+        s_milestones = _milestones;
     }
 
-    function setCountriesMaxSupply(
-        string[] memory _countries,
+    // 设置或更新里程碑的最大供应量
+    function _setMilestonesMaxSupply(
+        uint256[] memory _milestones,
         uint16 _maxSupply
-    ) public OnlyOwnerOrCollaborators {
-        if (_countries.length > s_milestones.length) {
-            revert LPCertNFT__InvalidMilestone(
-                "New countries length is out of range"
-            );
-        }
-        string[] memory countries = s_milestones;
-        for (uint256 i = 0; i < _countries.length; i++) {
-            if (!isValidCountry(_countries[i], countries)) {
-                revert LPCertNFT__InvalidMilestone(_countries[i]);
-            }
-            s_maxSupplyCountryNFT[_countries[i]] = _maxSupply;
+    ) internal onlyAuthorized {
+        for (uint256 i = 0; i < _milestones.length; i++) {
+            // if (!_isValidMilestone(_milestones[i])) {
+            //     revert LPCertNFT__InvalidMilestone(_milestones[i]);
+            // }
+            s_maxSupplyMilestoneNFT[_milestones[i]] = _maxSupply;
         }
     }
 
-    /* Modifiers */
+    /* 权限控制 */
 
-    // 合作伙伴地址到布尔值的映射，表示是否为合作伙伴
-    mapping(address => bool) internal s_collateralOwners;
+    // 定义操作员权限映射
+    mapping(address => bool) internal s_authorizedOperators;
 
-    modifier OnlyOwnerOrCollaborators() {
-        if (msg.sender != owner() && !s_collateralOwners[msg.sender]) {
-            revert LPCertNFT__OnlyOwnerOrCollateralOwner(msg.sender);
+    // 修饰符，限定函数只能由合约拥有者或授权的操作员调用
+    modifier onlyAuthorized() {
+        if (msg.sender != owner() && !s_authorizedOperators[msg.sender]) {
+            revert LPCertNFT__OnlyOwnerOrAuthorized(msg.sender);
         }
         _;
     }
 
-    // Add a new collaborator.
-    function addCollaborator(address _collaborator) external onlyOwner {
-        s_collateralOwners[_collaborator] = true;
+    // 添加授权操作员
+    function addAuthorizedOperator(address _operator) external onlyOwner {
+        s_authorizedOperators[_operator] = true;
     }
 
-    // Remove an existing collaborator.
-    function removeCollaborator(address _collaborator) external onlyOwner {
-        s_collateralOwners[_collaborator] = false;
+    // 移除授权操作员
+    function removeAuthorizedOperator(address _operator) external onlyOwner {
+        s_authorizedOperators[_operator] = false;
     }
 }
